fcb6ac2f993c267dcbdce172d4e781657f851c8e
kesava-reddym <kesava.reddym19@gmail.com>
2017-06-27 21:01:37 +0530

    Commit 1

---
+++ src/test/java/myPack/FacebookTest.java
@@ -0,0 +1,22 @@
+package myPack;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class FacebookTest {
+	
+  WebDriver driver;
+  @Test
+  public void facebookLoginTest1() {
+	  
+	  System.out.println("facebookLoginTest1");
+  }
+  
+  @Test
+public void facebookLoginTest2() {
+	  
+	  System.out.println("facebookLoginTest2");
+  }
+  
+  
+}
---
+++ src/test/java/myPack/GoogleTest.java
@@ -0,0 +1,15 @@
+package myPack;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTest 
+{
+	WebDriver driver;
+	  @Test
+	  public void googleTest() {
+		  
+		  System.out.println("googleTest");
+	  }
+	  
+}
